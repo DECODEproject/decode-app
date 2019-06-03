da96c7ae28fa235b0c956607a8cbe0b9b643f6d5
Jordi Buj <jordi.buj@jamgo.coop>
2019-06-03 17:07:03 +0200

    Redux Logger, Redux DevTools Extension, Hot reloading, React Native Debugger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./reducers', () => store.replaceReducer(rootReducer));
+}
+
+export default store;
